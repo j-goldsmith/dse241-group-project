e1250d2379438565eec111d88211af40aea2d639
Jared Goldsmith <jagoldsm@eng.ucsd.edu>
2018-01-09 21:52:48 -0800

    update viz structure, add base map

--- exercise_2/viz.js
+++ exercise_2/viz.js
@@ -1,44 +1,90 @@
 'use strict';
 
-var viz = function() {
+function map() {
+    var selectedCountries = [];
 
-    function display(element, data) {
-        var selectedCountries = [];
+    function display(selection) {
+        selection.each(function(data,i){
+            var rootElement = d3.select(this);
+            var projection = d3.geoMercator();
+            var path = d3.geoPath().projection(projection);
 
-        var map = (function (rootElement, data, selectedCountries) {
             rootElement
-                .append('svg')
-                    .attr('class', 'map')
-                .append('text')
-                    .attr('x',100)
-                    .attr('y',100)
-                    .text('Map Element!');
-        })(element, data, selectedCountries);
+                .append("g")
+                .attr("class", "countries")
+                .selectAll("path")
+                .data(data.atlas.features)
+                .enter().append("path")
+                .attr("d", path)
+               // .style("fill", function(d) { return color(populationById[d.id]); })
+                .style('stroke', 'white')
+                .style('stroke-width', 1.5)
+                .style("opacity",0.8)
 
-        var drilldown = (function (rootElement, data, selectedCountries) {
-            rootElement
-                .append('svg')
-                    .attr('class', 'drilldown')
-                .append('text')
-                    .attr('x',100)
-                    .attr('y',100)
-                    .text('Drilldown Element');
-        })(element, data, selectedCountries);
 
-    };
+            rootElement.append("path")
+                .datum(topojson.mesh(data.atlas.features, function(a, b) { return a.id !== b.id; }))
+                .attr("class", "names")
+                .attr("d", path);
 
-    function chart(selection) {
-        selection.each(function (data) {
-            display(
-                d3.select(this),
-                data
-            );
         });
     }
 
-    chart.selectCountries = function(){
-        return selectedCountries;
+    display.selectedCountries = function(value){
+        if (!arguments.length) return selectedCountries;
+        selectedCountries = value;
+        return display;
+    };
+
+    return display;
+}
+
+function drilldown() {
+    var selectedCountries = [];
+
+    function display(selection) {
+        selection.each(function(data,i) {
+            d3.select(this)
+                .append('text')
+                .attr('x', 100)
+                .attr('y', 100)
+                .text('Drilldown Element');
+        });
     }
 
-    return chart;
-}();
+    display.selectedCountries = function(value){
+        if (!arguments.length) return selectedCountries;
+        selectedCountries = value;
+        return display;
+    };
+
+    return display;
+}
+
+function viz() {
+    var selectedCountries = [];
+
+    function display(selection) {
+        selection.each(function (data, i) {
+            var rootElement = d3.select(this);
+
+            rootElement
+                .append('svg')
+                    .attr('class','map')
+                .call(map().selectedCountries(selectedCountries));
+
+            rootElement
+                .append('svg')
+                    .attr('class','map')
+                .call(drilldown().selectedCountries(selectedCountries));
+        });
+    }
+
+    display.selectedCountries = function(value){
+        if (!arguments.length) return selectedCountries;
+        selectedCountries = value;
+        return display;
+    };
+
+    return display;
+};
